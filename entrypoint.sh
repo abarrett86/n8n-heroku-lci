18a02d89084d919bfb9bffa436574b9e73aee37a
abarrett86 <abarrett86@gmail.com>
2025-04-07 18:59:39 -0700

    triggering build

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# check if port variable is set or go with default 
+# check if port variable is set or go with default
 if [ -z ${PORT+x} ]; then echo "PORT variable not defined, leaving N8N to default port."; else export N8N_PORT="$PORT"; echo "N8N will start on '$PORT'"; fi
 
 # regex function
